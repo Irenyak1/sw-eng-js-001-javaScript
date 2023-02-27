d3b89436d2c5b0c3345f203f1c99a7389a5dc9f0
“irenyak1” <inyakate@gmail.com>
2023-02-27 10:56:20 +0300

    Adds nested arrays and functions

--- 6arrays.js
+++ 6arrays.js
@@ -46,7 +46,7 @@ console.log(fruits);
 fruits.splice(2,0, 'orange', 'lemon'); // Adds two elements and removes zero
 console.log(fruits);
 
-let citrusFruits = fruits.slice(2,4);
+let citrusFruits = fruits.slice(2,4); // creates a sub array from an array
 console.log(fruits);
 console.log(citrusFruits);
 
@@ -56,3 +56,60 @@ let primeNumbers = [2,3,5]; // Array of prime numbers
 
 let allNumbers = evenNumbers.concat(oddNumbers, primeNumbers);
 console.log(allNumbers);
+
+
+
+
+// NESTED ARRAYS / MULTIDIMENSIONAL ARRAYS
+console.log("NESTED ARRAYS")
+let hobbies = [["swimming", 4], ["singing", 5], ["netball", 8], ["football", 6],]
+console.log("Print main array", hobbies)
+
+
+// PRINT OUT FIRST ITEM IN SECOND SUB-ARRAY
+console.log("print an element at specified index", hobbies[1][0])
+
+
+// REMOVE AN ELEMENT FROM SUB ARRAY
+hobbies[2].pop();
+console.log("After removing last element from a sub-array", hobbies)
+
+// ADDING ELEMENTS INTO THE SUB-ARRAY
+hobbies[1].push("hocky", 3);
+console.log("After push",hobbies)
+
+
+// DELETING FIRST SUB-ARRAY FROM THE MAIN ARRAY
+hobbies.splice(0,1) // delete first sub-array
+console.log("After deleting first sub-array", hobbies)
+
+
+// ADDING A SUB-ARRAY TO AN ARRAY AT A SPECIFIED POSITION
+//Here, the “hobbies.splice()” method will overwrite the “hobbies” array 
+// and add the “[‘dancing’, 3]” sub-array at the second position:
+hobbies.splice(1, 0, ['dancing', 3]); 
+console.log(hobbies)
+
+// DELETING LAST SUB-ARRAY FROM THE MAIN ARRAY
+hobbies.pop(); // Deletes last array from the main array
+console.log("After delete", hobbies)
+
+ 
+// To remove the second element of each “sub-array”, 
+// we will iterate through the “hobbies” array using the “forEach()” method, 
+// and at each iteration the “pop()” method deletes the element positioned at the first index:
+console.log("***********Using forEach function to loop through the elements in an array*****************")
+hobbies = [["swimming", 4], ["singing", 5], ["netball", 8], ["football", 6],]
+hobbies.forEach((hobby) => {
+    hobby.pop(1);
+});
+console.log("After delete second item from each nested array", hobbies)
+
+
+// FLATENING THE ARRAYS
+// Flaten the sub- arrays into one array
+// This removes all the sub arrays and creates one array
+hobbies = [["swimming", 4], ["singing", 5], ["netball", 8], ["football", 6],]
+const flatArray= hobbies.flat();
+console.log("Flatened array", flatArray)
+// console.log("Flat array", hobbies.flat())
---
+++ 7functions.js
@@ -0,0 +1,108 @@
+//  a function is a reusable block of code
+
+function funcName(){
+    //Function body/statements
+}
+
+// EXAMPLE
+function add(){   // none parameterized function
+    console.log(2+3);
+}
+add(); // function invocation
+
+// EXAMPLE
+function add2(a, b){  // parameterized function
+    // console.log(a+b);
+    return (a+b)
+}
+
+// add2(2,4); 
+console.log(add2(7,4)); 
+
+// EXAMPLE
+
+function add3(left, right) { // left & right are parameters here
+    let sum = left + right;   // left & right are arguments here
+    return sum;
+  }
+  add3();
+  console.log(add3(3, 6, 5)); // 5 is ignored; prints 9
+  console.log(add3(3)); // second argument missing; prints NaN
+                             // 3 + undefined is NaN
+
+
+// RETURN VALUES
+function add(a, b) {
+return a + b
+}
+
+add(2, 3); // returns 5
+
+
+// Function definition
+function say() {
+    console.log("How are you?");
+}
+  
+console.log("First");
+say(); // function invocation / calling a function
+console.log("Last");
+
+
+function Add(a, b){
+    return a + b
+}
+
+Add()
+console.log("----------------Adding values-----------------")
+console.log(Add(100000, 200000))
+
+console.log(Add(10000000000, 1898765433));
+
+console.log(Add(804850485048504, 497694760406404370));
+
+
+
+// Local and global variables
+
+// Local variable- has a limited scope (inside a function where it is defined)
+// A global variable - has a global scope (it can be accessed in any function)
+
+
+// EXAMPLE OF LOCAL VARIABLES
+function multiply(){
+    let localVariable1 = 5;
+    let localVariable2 = 6;
+    let product =  localVariable1 * localVariable2
+    return product
+
+}
+console.log("my product", multiply())
+
+
+function addition(){
+ let total = localVariable1 + localVariable2
+ return total 
+}
+
+console.log("Total", multiply2()) // This will throw an error of variables not defined
+
+
+// EXAMPLE OF GLOBAL VARIABLES
+let globalVariable1 = 5;
+let globalVariable2 = 6;
+
+function multiply2(){
+    let product =  globalVariable1 * globalVariable2
+    return product
+
+}
+console.log("my product", multiply())
+
+
+function addition2(){
+ let total = globalVariable1 + globalVariable2
+ return total 
+}
+
+console.log("Total", multiply2())
